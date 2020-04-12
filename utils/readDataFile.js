4d8bcae7e3520fe816d9572e9cad0195d725ac2a
afinana <antonio.finana@gmail.com>
2020-04-12 19:56:02 +0200

    Import opendata events and import in a local mongodb

--- utils/readDataFile.js
+++ utils/readDataFile.js
@@ -1,21 +1,38 @@
 ﻿/* Require dependencies */
-const csv = require('csv-stream');
+var https = require('https');
+var zlib = require('zlib');
 const fs = require('fs');
 
-/* We are reading pipe seperated values */
-const options = {
-    delimiter: '|'
-};
 
-module.exports = (file, callback, done) => {
+module.exports = (url, dest, cb) => {
+    var options = {
+     headers: {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8,ro;q=0.7,ru;q=0.6,la;q=0.5,pt;q=0.4,de;q=0.3',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36'
+        },
+        /* GZIP true for most of the websites now, disable it if you don't need it */
+        gzip: true,
+        followRedirect: true
+    
+    }
+    var file = fs.createWriteStream(dest);
     /* Create a new csv (or in this case psv) stream */
-    const csvStream = csv.createStream(options);
-    csvStream._encoding = 'utf8';
     /* Read the specified file and pipe the data into the psv stream */
-    fs.createReadStream(`${__dirname}/../${file}`).pipe(csvStream)
-        .on('data', (data) => {
-            /* Pass each entry to a callback function */
-            callback(data);
-        })
-        .on('end', done);
-};
+    var request = https.get(url, options, function (response) {
+        response.pipe(zlib.createGunzip()).pipe(file);
+        file.on('finish', function () {
+            file.close(cb);  // close() is async, call cb after close completes.
+           
+        });
+    }).on('error', function (err) { // Handle errors
+        console.error(err);
+        fs.unlink(cb); // Delete the file async. (But we don't check the result)
+      
+    });
+
+}
